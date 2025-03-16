e9ff37f1381cbd78b2acd761669b12c99d61f78c
Hafiz Aryan Siregar <117965934+hafizaryan@users.noreply.github.com>
2025-03-16 16:31:07 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,70 +8,6 @@
 
   html.className = html.className.replace(/\bno-js\b/g, "") + " js ";
 
-  /* Animations
-   * -------------------------------------------------- */
-  const tl = anime
-    .timeline({
-      easing: "easeInOutCubic",
-      duration: 800,
-      autoplay: false,
-    })
-    .add({
-      targets: "#loader",
-      opacity: 0,
-      duration: 1000,
-      begin: function (anim) {
-        window.scrollTo(0, 0);
-      },
-    })
-    .add({
-      targets: "#preloader",
-      opacity: 0,
-      complete: function (anim) {
-        document.querySelector("#preloader").style.visibility = "hidden";
-        document.querySelector("#preloader").style.display = "none";
-      },
-    })
-    .add(
-      {
-        targets: ".s-header",
-        translateY: [-100, 0],
-        opacity: [0, 1],
-      },
-      "-=200"
-    )
-    .add({
-      targets: [".s-intro .text-pretitle", ".s-intro .text-huge-title"],
-      translateX: [100, 0],
-      opacity: [0, 1],
-      delay: anime.stagger(400),
-    })
-    .add({
-      targets: ".circles span",
-      keyframes: [
-        { opacity: [0, 0.3] },
-        {
-          opacity: [0.3, 0.1],
-          delay: anime.stagger(100, { direction: "reverse" }),
-        },
-      ],
-      delay: anime.stagger(100, { direction: "reverse" }),
-    })
-    .add({
-      targets: ".intro-social li",
-      translateX: [-50, 0],
-      opacity: [0, 1],
-      delay: anime.stagger(100, { direction: "reverse" }),
-    })
-    .add(
-      {
-        targets: ".intro-scrolldown",
-        translateY: [100, 0],
-        opacity: [0, 1],
-      },
-      "-=800"
-    );
-
   /* Preloader
    * -------------------------------------------------- */
   const ssPreloader = function () {
@@ -82,17 +18,9 @@
       document.querySelector("html").classList.remove("ss-preload");
       document.querySelector("html").classList.add("ss-loaded");
 
-      document.querySelectorAll(".ss-animated").forEach(function (item) {
-        item.classList.remove("ss-animated");
-      });
-
-      tl.play();
+      document.querySelector("#preloader").style.visibility = "hidden";
+      document.querySelector("#preloader").style.display = "none";
     });
-
-    // force page scroll position to top at page refresh
-    // window.addEventListener('beforeunload' , function () {
-    //     // window.scrollTo(0, 0);
-    // });
   }; // end ssPreloader
 
   /* Mobile Menu
@@ -168,42 +96,6 @@
       });
     }
   }; // end ssScrollSpy
-
-  /* Animate elements if in viewport
-   * ------------------------------------------------------ */
-  const ssViewAnimate = function () {
-    const blocks = document.querySelectorAll("[data-animate-block]");
-
-    window.addEventListener("scroll", viewportAnimation);
-
-    function viewportAnimation() {
-      let scrollY = window.pageYOffset;
-
-      blocks.forEach(function (current) {
-        const viewportHeight = window.innerHeight;
-        const triggerTop =
-          current.offsetTop + viewportHeight * 0.2 - viewportHeight;
-        const blockHeight = current.offsetHeight;
-        const blockSpace = triggerTop + blockHeight;
-        const inView = scrollY > triggerTop && scrollY <= blockSpace;
-        const isAnimated = current.classList.contains("ss-animated");
-
-        if (inView && !isAnimated) {
-          anime({
-            targets: current.querySelectorAll("[data-animate-el]"),
-            opacity: [0, 1],
-            translateY: [100, 0],
-            delay: anime.stagger(400, { start: 200 }),
-            duration: 800,
-            easing: "easeInOutCubic",
-            begin: function (anim) {
-              current.classList.add("ss-animated");
-            },
-          });
-        }
-      });
-    }
-  }; // end ssViewAnimate
 
   /* Swiper
    * ------------------------------------------------------ */
@@ -314,7 +206,7 @@
     const moveTo = new MoveTo(
       {
         tolerance: 0,
-        duration: 1200,
+        duration: 600,
         easing: "easeInOutCubic",
         container: window,
       },
@@ -332,7 +224,6 @@
     ssPreloader();
     ssMobileMenu();
     ssScrollSpy();
-    ssViewAnimate();
     ssSwiper();
     ssLightbox();
     ssAlertBoxes();
